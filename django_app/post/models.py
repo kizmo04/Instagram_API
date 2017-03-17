11cabd01427a3fa3c21dd09cfe6a8e4d876ee88d
kizmo04 <kizmo04@gmail.com>
2017-03-17 14:53:45 +0900

    PostPhoto모델 수정 및 migration생성

--- django_app/post/models.py
+++ django_app/post/models.py
@@ -7,24 +7,14 @@ class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL)
     created_date = models.DateTimeField(auto_now_add=True)
 
-    def to_dict(self):
-        return {
-            'pk': self.pk,
-            'created_date': self.created_date,
-            'author_list': self.author.to_dict(),
-            'photo_list': [photo.to_dict() for photo in self.postphoto_set.all()]
-        }
+    class Meta:
+        ordering = ('-pk',)
 
 
 class PostPhoto(models.Model):
     post = models.ForeignKey(Post)
     photo = models.ImageField(upload_to='post')
+    created_date = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         order_with_respect_to = 'post'
-
-    def to_dict(self):
-        return {
-            'pk': self.pk,
-            'photo': self.photo.url,
-        }
