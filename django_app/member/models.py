ab983688547d8023711536ec0077f9d486a773a1
kizmo04 <kizmo04@gmail.com>
2017-03-17 17:31:34 +0900

    MyUser의 relation필드 추가 및 IntermediateModel Relationship추가, migration 생성

--- django_app/member/models.py
+++ django_app/member/models.py
@@ -4,7 +4,13 @@ from django.db import models
 
 class MyUser(AbstractUser):
     img_profile = models.ImageField(upload_to='member')
-
+    relation = models.ManyToManyField(
+        'self',
+        symmetrical=False,
+        related_name='relation_user_set',
+        through='Relationship',
+    )
+    
     def to_dict(self):
         return {
             'pk': self.pk,
@@ -12,3 +18,29 @@ class MyUser(AbstractUser):
             'first_name': self.first_name,
             'last_name': self.last_name,
         }
+
+
+class Relationship(models.Model):
+    RELATIONSHIP_FOLLOWING = 1
+    RELATIONSHIP_BLOCKED = 2
+    RELATIONSHIP_TYPE = (
+        (RELATIONSHIP_FOLLOWING, 'Follow'),
+        (RELATIONSHIP_BLOCKED, 'Block'),
+    )
+
+    from_user = models.ForeignKey(MyUser, related_name='relations_from_user')
+    to_user = models.ForeignKey(MyUser, related_name='relations_to_user')
+    relation_type = models.IntegerField(choices=RELATIONSHIP_TYPE)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = (
+            ('from_user', 'to_user')
+        )
+
+    def __str__(self):
+        return 'Relation({}) From({}) To ({})'.format(
+            self.get_relation_type_display(),
+            self.from_user.username,
+            self.to_user.username,
+        )
