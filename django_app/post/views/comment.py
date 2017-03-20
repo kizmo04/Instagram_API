cca78b43a5183471204845ca01d831bd8c5631fa
kizmo04 <kizmo04@gmail.com>
2017-03-20 18:15:39 +0900

    class based view 로 CommentCreate구현

--- django_app/post/views/comment.py
+++ django_app/post/views/comment.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import redirect
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.utils.decorators import method_decorator
-from django.views import View
+from django.views.generic import CreateView
 
 from post.models import Post
 from post.models import PostComment
@@ -12,15 +13,21 @@ __all__ = (
 
 
 @method_decorator(login_required, name='dispatch')
-class CommentCreate(View):
-    def post(self, request, *args, **kwargs):
-        post_pk = kwargs['post_pk']
+class CommentCreate(CreateView):
+    model = PostComment
+    fields = ('content',)
+
+    def get_success_url(self):
+        next = self.request.GET.get('next')
+        return next if next else reverse('post:post-detail', kwargs={'pk': self.kwargs['post_pk']})
+
+    def form_valid(self, form):
+        # author 와 post가 빈 PostComment 객체 생성
+        comment = form.save(commit=False)
+        post_pk = self.kwargs['post_pk']
         post = Post.objects.get(pk=post_pk)
-        content = request.POST['content']
-        author = request.user
-        PostComment.objects.create(
-            post=post,
-            author=author,
-            content=content,
-        )
-        return redirect('post:post-detail', pk=post_pk)
+        author = self.request.user
+        comment.post = post
+        comment.author = author
+        comment.save()
+        return HttpResponseRedirect(self.get_success_url())
