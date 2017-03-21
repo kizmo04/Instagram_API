43688cd081e3cca8e3a5825583781328ce65ab32
kizmo04 <kizmo04@gmail.com>
2017-03-21 14:49:49 +0900

    PostCreate post메서드 구현, swiper에 autoHeight옵션 추가, PostCreate뷰 GET메서드 구현 및 연결

--- django_app/post/urls/views.py
+++ django_app/post/urls/views.py
@@ -7,7 +7,7 @@ urlpatterns = [
     url(r'^$', views.PostList.as_view(), name='post-list'),
     url(r'^(?P<pk>[0-9]+)/$', views.PostDetail.as_view(), name='post-detail'),
     url(r'^(?P<pk>[0-9]+)/delete/$', views.PostDelete.as_view(), name='post-delete'),
-    url(r'^(?P<pk>[0-9]+)/create/$', views.PostCreate.as_view(), name='post-create'),
+    url(r'^create/$', views.PostCreate.as_view(), name='post-create'),
     url(r'^(?P<post_pk>[0-9]+)/comment/add/$', views.CommentCreate.as_view(), name='comment-add'),
 
 ]
--- django_app/post/views/post.py
+++ django_app/post/views/post.py
@@ -1,11 +1,16 @@
 """
 class based view로 PostList, PostDetail, PostCreate, PostDelete 뷰를 작성
 """
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from django.views import View
 from django.views.generic import DetailView
 from django.views.generic import ListView
 
+from post.forms import PostForm
 from post.models import Post
+from post.models import PostComment
+from post.models import PostPhoto
 
 __all__ = (
     'PostList',
@@ -25,7 +30,31 @@ class PostDetail(DetailView):
 
 
 class PostCreate(View):
-    pass
+    form_class = PostForm
+    template_name = 'post/post_create.html'
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class()
+        context = {
+            'form': form,
+        }
+        return render(request, self.template_name, context)
+
+    def post(self, request, *args, **kwargs):
+        print(request.POST)
+        print(request.FILES)
+        post = Post.objects.create(author=request.user)
+        form = self.form_class(request.POST, request.FILES)
+        if form.is_valid():
+            content = form.cleaned_data.get('content', '').strip()
+            if content != '':
+                PostComment.objects.create(content=content, post=post, author=request.user)
+            for photo in request.FILES.getlist('photos'):
+                PostPhoto.objects.create(post=post, photo=photo)
+
+            return redirect('post:post-list')
+        else:
+            return HttpResponse(form.errors)
 
 
 class PostDelete(View):
